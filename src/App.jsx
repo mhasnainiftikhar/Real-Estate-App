a83ea4368845f228fb8e196f428d3a6f14ab307f
mhasnainiftikhar <sp21-bse-014@cuilahore.edu.pk>
2025-03-25 16:26:03 +0500

    Pages & Routes

--- src/App.jsx
+++ src/App.jsx
@@ -1,18 +1,20 @@
-import { useState } from "react";
+import { BrowserRouter, Routes, Route } from "react-router-dom";
+import Home from "./pages/Home";
+import SignIn from "./pages/SignIn";
+import SignUp from "./pages/SignUp";
+import About from "./pages/About";
+import Profile from "./pages/Profile";
 
-function App() {
-  const [count, setCount] = useState(0);
 
-  return (
-    <>
-      <h1
-        class="text-3xl font-bold underline 
-        hover:text-blue-500 transition-colors duration-300 "
-      >
-        Hello world!
-      </h1>
-    </>
-  );
+
+export default function App() {
+  return <BrowserRouter>
+    <Routes>
+      <Route path="/" element={<Home />} />
+      <Route path="/sign-in" element={<SignIn />} />
+      <Route path="/sign-up" element={<SignUp />} />
+      <Route path="/about" element={<About />} />
+      <Route path="/profile"  element={<Profile/>}/>
+    </Routes>
+  </BrowserRouter>;
 }
-
-export default App;
